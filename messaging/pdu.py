190a994fb4614ccad0cd386d3d93a1eb195552db
Pablo Marti <pmarti@warp.es>
2009-01-17 13:38:05 +0100

    pep8 fixes

--- messaging/pdu.py
+++ messaging/pdu.py
@@ -28,7 +28,8 @@ UNICODE_FORMAT  = 0x08
 
 from random import shuffle
 
-class PDU :
+class PDU(object):
+
     def __init__(self):
         self.random_id_list = range(0, 255)
         shuffle(self.random_id_list)
@@ -48,23 +49,23 @@ class PDU :
         SMSC can be ommited if phone can use internal defaults
         """
 
-        smsc_pdu = self.__get_smsc_pdu(csca)
-        sms_submit_pdu = self.__get_sms_submit_pdu(request_status, msgvp)
-        tpmessref_pdu = self.__get_tpmessref_pdu(msgref)
-        sms_phone_pdu = self.__get_phone_pdu(number)
-        tppid_pdu = self.__get_tppid_pdu()
-        sms_msg_pdu = self.__get_msg_pdu(text, msgvp)
+        smsc_pdu = self._get_smsc_pdu(csca)
+        sms_submit_pdu = self._get_sms_submit_pdu(request_status, msgvp)
+        tpmessref_pdu = self._get_tpmessref_pdu(msgref)
+        sms_phone_pdu = self._get_phone_pdu(number)
+        tppid_pdu = self._get_tppid_pdu()
+        sms_msg_pdu = self._get_msg_pdu(text, msgvp)
 
-        if len(sms_msg_pdu) == 1 :
-            print "smsc_pdu : %s" % smsc_pdu
-            print "sms_submit_pdu : %s" % sms_submit_pdu
-            print "tpmessref_pdu : %s" % tpmessref_pdu
-            print "sms_phone_pdu : %s" % sms_phone_pdu
-            print "tppid_pdu : %s" % tppid_pdu
-            print "sms_msg_pdu : %s" % sms_msg_pdu
+        if len(sms_msg_pdu) == 1:
+            print "smsc_pdu: %s" % smsc_pdu
+            print "sms_submit_pdu: %s" % sms_submit_pdu
+            print "tpmessref_pdu: %s" % tpmessref_pdu
+            print "sms_phone_pdu: %s" % sms_phone_pdu
+            print "tppid_pdu: %s" % tppid_pdu
+            print "sms_msg_pdu: %s" % sms_msg_pdu
             print "--------------------------------------------"
             pdu = smsc_pdu
-            len_smsc = len(smsc_pdu)/2
+            len_smsc = len(smsc_pdu) / 2
             pdu += sms_submit_pdu
             pdu += tpmessref_pdu
             pdu += sms_phone_pdu
@@ -72,20 +73,21 @@ class PDU :
             pdu += sms_msg_pdu[0]
             print pdu
             print "--------------------------------------------"
-            return [((len(pdu)/2)-len_smsc, pdu.upper())]
+            return [((len(pdu) / 2) - len_smsc, pdu.upper())]
         else:
-            sms_submit_pdu = self.__get_sms_submit_pdu(request_status, msgvp, udh=True)
+            sms_submit_pdu = self._get_sms_submit_pdu(request_status, msgvp,
+                                                      udh=True)
             pdu_list = []
-            for sms_msg_pdu_item in sms_msg_pdu :
-                print "smsc_pdu : %s" % smsc_pdu
-                print "sms_submit_pdu : %s" % sms_submit_pdu
-                print "tpmessref_pdu : %s" % tpmessref_pdu
-                print "sms_phone_pdu : %s" % sms_phone_pdu
-                print "tppid_pdu : %s" % tppid_pdu
-                print "sms_msg_pdu : %s" % sms_msg_pdu_item
+            for sms_msg_pdu_item in sms_msg_pdu:
+                print "smsc_pdu: %s" % smsc_pdu
+                print "sms_submit_pdu: %s" % sms_submit_pdu
+                print "tpmessref_pdu: %s" % tpmessref_pdu
+                print "sms_phone_pdu: %s" % sms_phone_pdu
+                print "tppid_pdu: %s" % tppid_pdu
+                print "sms_msg_pdu: %s" % sms_msg_pdu_item
                 print "--------------------------------------------"
                 pdu = smsc_pdu
-                len_smsc = len(smsc_pdu)/2
+                len_smsc = len(smsc_pdu) / 2
                 pdu += sms_submit_pdu
                 pdu += tpmessref_pdu
                 pdu += sms_phone_pdu
@@ -93,13 +95,14 @@ class PDU :
                 pdu += sms_msg_pdu_item
                 print pdu
                 print "--------------------------------------------"
-                pdu_list.append(((len(pdu)/2)-len_smsc, pdu.upper()))
-            return pdu_list
+                pdu_list.append(((len(pdu) / 2) - len_smsc, pdu.upper()))
 
+            return pdu_list
 
     def decode_pdu(self, pdu):
         """
         Decodes a complete SMS pdu and returns a tuple strings
+
         sender,  # Senders number
         datestr, # GSM format date string
         msg,     # The actual msg less any header in UCS2 format
@@ -145,6 +148,7 @@ class PDU :
         if sndlen % 2:
             sndlen += 1
         sndtype = pdu[ptr+2:ptr+4]
+
         # Extract Phone number of sender
         sender = pdu[ptr+4:ptr+4+sndlen]
         sender = sender.replace('F', '')
@@ -164,7 +168,7 @@ class PDU :
         # 1 byte TP-DCS (Data Coding Scheme)
         DCS = int(pdu[ptr:ptr+2], 16)
         fmt = SEVENBIT_FORMAT
-        if DCS & (EIGHTBIT_FORMAT|UNICODE_FORMAT) == 0:
+        if DCS & (EIGHTBIT_FORMAT | UNICODE_FORMAT) == 0:
             fmt = SEVENBIT_FORMAT
         elif DCS & EIGHTBIT_FORMAT:
             fmt = EIGHTBIT_FORMAT
@@ -183,10 +187,8 @@ class PDU :
         msgl = int(pdu[ptr:ptr+2], 16)
         msg = pdu[ptr+2:]
         # check for header
-        cnt = 0
-        seq = 0
-        ref = 0
-        headlen = 0
+        cnt = seq = ref = headlen = 0
+
         if testheader:
             if msg[2:4] == "00": # found header for concat message
                 headlen = (int(msg[0:2], 16) + 1) * 8
@@ -200,7 +202,7 @@ class PDU :
                     headlen = headlen / 7
 
         if fmt == SEVENBIT_FORMAT:
-            msg = self.__unpack_msg(msg)[headlen:msgl]
+            msg = self._unpack_msg(msg)[headlen:msgl]
             msg = msg.decode("gsm0338")
 
         elif fmt == EIGHTBIT_FORMAT:
@@ -216,40 +218,46 @@ class PDU :
 
     #Private methods
 
-    def __get_smsc_pdu (self, number):
+    def _get_smsc_pdu(self, number):
         if not len(number.strip()):
             return chr(0)
-        number = self.__clean_number(number)
+
+        number = self._clean_number(number)
         ptype = 129
         if number[0] == '+':
             number = number[1:]
             ptype = 145
+
         if len(number) % 2:
             number = number + 'F'
+
         ps = chr(ptype)
         for n in range(0, len(number), 2):
             num = number[n+1] + number[n]
             ps = ps + chr(int(num, 16))
+
         pl = len(ps)
         ps = chr(pl) + ps
 
         ret_ps = ''.join(["%02x" % ord(n) for n in ps])
         return ret_ps
 
-    def __get_tpmessref_pdu(self, msgref):
+    def _get_tpmessref_pdu(self, msgref):
         tpmessref = msgref & 0xFF
         ret_tpmessref = ''.join(["%02x" % ord(n) for n in chr(tpmessref)])
         return ret_tpmessref
 
-    def __get_phone_pdu(self, number):
-        number = self.__clean_number(number)
+    def _get_phone_pdu(self, number):
+        number = self._clean_number(number)
         ptype = 129
         if number[0] == '+':
             number = number[1:]
             ptype = 145
+
         pl = len(number)
         if len(number) % 2:
             number = number + 'F'
+
         ps = chr(ptype)
         for n in range(0, len(number), 2):
             num = number[n+1] + number[n]
@@ -260,106 +268,99 @@ class PDU :
         ret_ps = ''.join(["%02x" % ord(n) for n in ps])
         return ret_ps
 
-    def __clean_number(self, number):
+    def _clean_number(self, number):
         number = number.strip()
-        number = number.replace(' ', '')
-        return number
+        return number.replace(' ', '')
 
-    def __get_tppid_pdu(self):
+    def _get_tppid_pdu(self):
         tppid = 0x00
         ret_tppid = ''.join(["%02x" % ord(n) for n in chr(tppid)])
         return ret_tppid
 
-    def __get_sms_submit_pdu(self, request_status, msgvp, udh=False):
-        sms_submit = None
-        if not request_status:
-            sms_submit = 0x01
-        else:
-            sms_submit = 0x21
+    def _get_sms_submit_pdu(self, request_status, msgvp, udh=False):
+        sms_submit = 0x01 if not request_status else 0x21
 
         if msgvp != 0xFF:
             sms_submit = sms_submit | 0x10
-
-        if udh :
+        if udh:
             sms_submit = sms_submit | 0x40
 
         ret_sms_submit = ''.join(["%02x" % ord(n) for n in chr(sms_submit)])
         return ret_sms_submit
 
-
-    def __get_msg_pdu(self, text, msgvp):
-        #Data coding Scheme
-        data_coding_scheme = ""
+    def _get_msg_pdu(self, text, validity_period):
+        # Data coding scheme
+        dcs = ""
         if gsm0338.is_valid_gsm_text(text):
             text_format = SEVENBIT_FORMAT
-            data_coding_scheme = 0x00 | SEVENBIT_FORMAT
+            dcs = 0x00 | SEVENBIT_FORMAT
         else:
             text_format = UNICODE_FORMAT
-            data_coding_scheme = 0x00 | UNICODE_FORMAT
+            dcs = 0x00 | UNICODE_FORMAT
 
-        data_coding_scheme_pdu = ''.join(["%02x" % ord(n) for n in chr(data_coding_scheme)])
+        dcs_pdu = ''.join(["%02x" % ord(n) for n in chr(dcs)])
 
         #Validity period
-        valid_period = msgvp & 0xFF
-        valid_period_pdu = ''.join(["%02x" % ord(n) for n in chr(valid_period)])
+        msgvp = validity_period & 0xFF
+        msgvp_pdu = ''.join(["%02x" % ord(n) for n in chr(msgvp)])
 
         #UDL + UD
         message_pdu = ""
 
-        if text_format == SEVENBIT_FORMAT :
-            if len(text) <= 160 :
+        if text_format == SEVENBIT_FORMAT:
+            if len(text) <= 160:
                 print "GSM0338 (len %s)" % len(text)
-                message_pdu = [self.__pack_8bits_to_7bits(text)]
+                message_pdu = [self._pack_8bits_to_7bits(text)]
             else:
                 print "GSM0338 (len %s multipart)" % len(text)
-                message_pdu = self.__split_sms_message(text, encoding=SEVENBIT_FORMAT,
-                                                       limit=160)
+                message_pdu = self._split_sms_message(text, limit=160,
+                                                      encoding=SEVENBIT_FORMAT)
         else:
-            if len(text) <= 70 :
+            if len(text) <= 70:
                 print "UNICODE (len %s)" % len(text)
-                message_pdu = [self.__pack_8bits_to_ucs2(text)]
-
+                message_pdu = [self._pack_8bits_to_ucs2(text)]
             else:
                 print "UNICODE (len %s)" % len(text)
-                message_pdu = self.__split_sms_message(text, encoding=UNICODE_FORMAT,
-                                                       limit=70)
+                message_pdu = self._split_sms_message(text, limit=70,
+                                                      encoding=UNICODE_FORMAT)
 
         ret_msgs_list = []
 
-        for msg in message_pdu :
-            ret_msgs_list.append(data_coding_scheme_pdu + valid_period_pdu + msg)
+        for msg in message_pdu:
+            ret_msgs_list.append(dcs_pdu + msgvp_pdu + msg)
 
         return ret_msgs_list
 
-    def __pack_8bits_to_ucs2(self, message, udh=None):
-        #FIXME : ESTO NO CONTROLA EN TAMAÑO EN BASE AL UDH
+    def _pack_8bits_to_ucs2(self, message, udh=None):
+        # FIXME: ESTO NO CONTROLA EN TAMAÑO EN BASE AL UDH
         text = message
         nmesg = ''
         for n in text:
             nmesg += chr(ord(n) >> 8) + chr(ord(n) & 0xFF)
+
         mlen = len(text) * 2
-        if udh == None:
-            message= chr(mlen) + nmesg
+        if udh is None:
+            message = chr(mlen) + nmesg
         else:
-            message= chr(mlen) + udh + nmesg
+            message = chr(mlen) + udh + nmesg
 
         message_pdu = ''.join(["%02x" % ord(n) for n in message])
         return message_pdu
 
-    def __pack_8bits_to_7bits(self, message, udh=None):
+    def _pack_8bits_to_7bits(self, message, udh=None):
         pdu = ""
         txt = message.encode("gsm0338")
 
         if udh == None:
             tl = len(txt)
             txt += '\x00'
-            msgl = len(txt)*7/8
+            msgl = len(txt) * 7 / 8
             op = [-1] * msgl
-            c = 0
-            shift = 0
+            c = shift = 0
+
             for n in range(msgl):
                 if shift == 6:
-                    c = c + 1
+                    c += 1
 
                 shift = n % 7
                 lb = ord(txt[c]) >> shift
@@ -372,10 +373,10 @@ class PDU :
             tl = len(txt)
 
             txt += '\x00'
-            msgl = len(txt)*7/8
+            msgl = len(txt) * 7 / 8
             op = [-1] * msgl
-            c = 0
-            shift = 0
+            c = shift = 0
+
             for n in range(msgl):
                 if shift == 6:
                     c = c + 1
@@ -386,33 +387,30 @@ class PDU :
                 op[n] = lb + hb
                 c += 1
 
-            i=0
-            for char in udh :
+            for i, char in enumerate(udh):
                 op[i] = ord(char)
-                i = i + 1
 
             pdu = chr(tl) + ''.join([chr(x) for x in op])
-
 
         ret_pdu = ''.join(["%02x" % ord(n) for n in pdu])
         return ret_pdu
 
-    def __split_sms_message(self, text, encoding=SEVENBIT_FORMAT, limit=160):
+    def _split_sms_message(self, text, encoding=SEVENBIT_FORMAT, limit=160):
         len_without_udh = limit - 7
         msgs = []
         total_len = len(text)
-        pi=0
-        pe=len_without_udh
-        while pi < total_len :
+        pi = 0
+        pe = len_without_udh
+        while pi < total_len:
             msgs.append(text[pi:pe])
-            pi=pe
-            pe=pe+len_without_udh
+            pi = pe
+            pe += len_without_udh
 
         packing_func = None
         if encoding == SEVENBIT_FORMAT:
-            packing_func = self.__pack_8bits_to_7bits
+            packing_func = self._pack_8bits_to_7bits
         else:
-            packing_func = self.__pack_8bits_to_ucs2
+            packing_func = self._pack_8bits_to_ucs2
 
         pdu_msgs = []
 
@@ -420,40 +418,45 @@ class PDU :
         udh_len = 0x05
         mid = 0x00
         data_len = 0x03
-        csms_ref = self.__get_rand_id()
+        csms_ref = self._get_rand_id()
 
-        for msg in msgs :
+        for msg in msgs:
             total_parts = len(msgs)
-            udh = chr(udh_len) + chr(mid) + chr(data_len) + chr(csms_ref) + chr(total_parts) + chr(i)
-            pdu_msgs.append(packing_func(" "+msg, udh))
-            i=i+1
+            udh = (chr(udh_len) + chr(mid) + chr(data_len) + chr(csms_ref) +
+                    chr(total_parts) + chr(i))
+            pdu_msgs.append(packing_func(" " + msg, udh))
+            i += 1
 
         return pdu_msgs
 
-    def __unpack_msg(self, pdu, limit=160):
+    def _unpack_msg(self, pdu, limit=160):
         """
         Unpacks C{pdu} into 7-bit characters and returns the decoded string
         """
         # Taken/modified from Dave Berkeley's pysms package
-        count = 0
-        last = 0
+        count = last = 0
         result = []
+
         for i in range(0, len(pdu), 2):
             byte = int(pdu[i:i+2], 16)
             mask = 0x7F >> count
             out = ((byte & mask) << count) + last
             last = byte >> (7 - count)
             result.append(chr(out))
+
             if limit and (len(result) >= limit):
                 break
+
             if count == 6:
                 result.append(chr(last))
                 last = 0
 
             count = (count + 1) % 7
+
         return ''.join(result)
 
-    def __get_rand_id(self):
+    def _get_rand_id(self):
         rid = self.random_id_list.pop()
         self.random_id_list.insert(0, rid)
         return rid
+
