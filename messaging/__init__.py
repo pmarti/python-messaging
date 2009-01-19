0b5e5cf3b78b5e987d588a4bb66eddbeac856aea
Pablo Martí <pmarti@warp.es>
2009-01-19 09:58:32 +0100

    syntactic sugar for imports

--- messaging/__init__.py
+++ messaging/__init__.py
@@ -1,1 +1,4 @@
-from pdu import PDU
+from messaging.pdu import PDU, SEVENBIT_SIZE, UCS2_SIZE
+from messaging.gsm0338 import is_valid_gsm_text
+
+__all__ = ["PDU", "SEVENBIT_SIZE", "UCS2_SIZE", "is_valid_gsm_text"]
