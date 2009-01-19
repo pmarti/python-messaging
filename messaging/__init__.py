59814a4eba00849529a908184ea8a185492d39bb
Pablo Martí <pmarti@warp.es>
2009-01-19 14:25:30 +0100

    Max length for multipart 7bit and ucs2 packages as constant

--- messaging/__init__.py
+++ messaging/__init__.py
@@ -1,4 +1,6 @@
-from messaging.pdu import PDU, SEVENBIT_SIZE, UCS2_SIZE
+from messaging.pdu import (PDU, SEVENBIT_SIZE, UCS2_SIZE, SEVENBIT_MP_SIZE,
+                           UCS2_MP_SIZE)
 from messaging.gsm0338 import is_valid_gsm_text
 
-__all__ = ["PDU", "SEVENBIT_SIZE", "UCS2_SIZE", "is_valid_gsm_text"]
+__all__ = ["PDU", "SEVENBIT_SIZE", "SEVENBIT_MP_SIZE", "UCS2_SIZE",
+           "UCS2_MP_SIZE", "is_valid_gsm_text"]
