4a34e394f10706ab92cbfe05eb3682a99481631e
Pablo Marti <pmarti@warp.es>
2010-04-16 18:59:03 +0200

    Replace setuptools with distribute

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from ez_setup import use_setuptools; use_setuptools()
+from distribute_setup import use_setuptools
+use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name="python-messaging",
