7fafce7a98e568dc340686a88ae562e548b7eb33
Pablo Martí <pmarti@warp.es>
2010-04-20 09:10:42 +0200

    Export commonly used functions and constants

--- messaging/__init__.py
+++ messaging/__init__.py
@@ -1,4 +1,8 @@
-from messaging.pdu import PDU
+from messaging.pdu import (PDU, SEVENBIT_SIZE, UCS2_SIZE, UCS2_MP_SIZE,
+                           SEVENBIT_MP_SIZE, is_valid_gsm_text)
 
 VERSION = (0, 3, 0)
-__all__ = ["PDU"]
+
+__all__ = ["PDU", "SEVENBIT_SIZE", "UCS2_SIZE", "SEVENBIT_SIZE",
+           "UCS2_MP_SIZE", "SEVENBIT_MP_SIZE", "is_valid_gsm_text"]
+
